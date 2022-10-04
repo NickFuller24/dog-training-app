a62ab85e62d3fa2e8af9cb909fc77d9ebf003152
Jack Stork <jksd2d@umsystem.edu>
2022-10-04 16:34:50 -0500

    Revert "First - just getting some simple folders set up"
    This reverts commit b1c21b2865e9b29f8ed4a437b98d855403ad9fb0.

--- dog_training_app/App.tsx
+++ dog_training_app/App.tsx
@@ -1,7 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
-import LoginScreen from './app/screens/LoginScreen';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return <LoginScreen />;
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
