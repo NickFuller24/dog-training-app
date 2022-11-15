2d4fa0d23694d94ea464dede512907cb3b3256a1
Nick Fuller <24nickfuller24@gmail.com>
2022-11-14 18:59:58 -0600

    test

--- dog_training_app/app/screens/LoginScreen.tsx
+++ dog_training_app/app/screens/LoginScreen.tsx
@@ -6,10 +6,7 @@ function LoginScreen({ navigation }: { navigation: any }) {
   return (
     <View style={styles.background}>
       <View style={styles.logoContainer}>
-        <Image
-          style={styles.logoImage}
-          source={require("../assets/loginDogImage.png")}
-        />
+        <Image style={styles.logoImage} source={require("../assets/loginDogImage.png")}/>
         <Text style={styles.logoText}>Train your dog!</Text>
       </View>
       <View style={styles.buttonContainer}>
@@ -18,13 +15,6 @@ function LoginScreen({ navigation }: { navigation: any }) {
           onPress={() => navigation.navigate("Sign In")}
         >
           <Text style={styles.buttonText}>Sign In</Text>
-        </Pressable>
-        <View style={styles.buttonSpace} />
-        <Pressable
-          style={styles.loginButton}
-          onPress={() => navigation.navigate("Dog Profile")}
-        >
-          <Text style={styles.buttonText}>Dog</Text>
         </Pressable>
         <View style={styles.buttonSpace} />
         <Pressable
