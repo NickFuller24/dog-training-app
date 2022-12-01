22f154b6897142c89a46743543e38c819c459751
Nick Fuller <24nickfuller24@gmail.com>
2022-11-30 22:01:58 -0600

    Created navbar and background components. They aren't perfect but they're a good start

--- dog_training_app/metro.config.js
+++ dog_training_app/metro.config.js
@@ -1,4 +1,21 @@
 const { getDefaultConfig } = require("@expo/metro-config");
-const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.assetExts.push("cjs");
-module.exports = defaultConfig;
+
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
+
+  config.resolver.assetExts.push("cjs");
+
+  return config;
+})();
